6057870e68f39906f6bd27479351b174550665e9
jorj <167171844+jorjsorrows@users.noreply.github.com>
2024-04-23 18:17:33 +0530

    Update user.go

--- user.go
+++ user.go
@@ -14,7 +14,7 @@ import (
 var DB *gorm.DB
 var err error
 
-const DNS = "root:iwillgetajob36@tcp(127.0.0.1:3306)/songs?charset=utf8mb4&parseTime=True&loc=Local"
+const DNS = "root:(PASSWORD)@tcp(127.0.0.1:3306)/songs?charset=utf8mb4&parseTime=True&loc=Local"
 
 type Songs struct {
 	gorm.Model
